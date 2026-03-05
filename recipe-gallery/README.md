0f2499b81dd6b2c7856ff73decee438d1025a030
ellieNoble13 <ellie.noble@seattlecolleges.edu>
2026-03-05 15:14:39 -0800

    built app and updated readme

---
+++ recipe-gallery/README.md
@@ -0,0 +1,4 @@
+This is a simple showcase of vite and react.
+
+You can check out my concept for a recipe card website
+that uses CSS to create a UI that feels 90s, but UX that feels modern
---
+++ recipe-gallery/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RecipeGallery from './RecipeGallery';
+import './App.css';
+function App() {
+    return (
+        <div className="App">
+            <RecipeGallery />
+        </div>
+    );
+}
+
+export default App
